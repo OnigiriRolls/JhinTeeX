bb5d8819dffa2a95c018b69fbecab28f7b125788
Onigiri <kethrinlie@yahoo.com>
2023-04-02 00:12:15 +0300

    Added pause button, menus

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public RectTransform panel;
+    public Rigidbody2D player;
+    public Button button;
+    public Image img;
+
+    public void OnPause()
+    {
+        panel.gameObject.SetActive(true);
+        player.constraints = RigidbodyConstraints2D.FreezePosition;
+        button.gameObject.SetActive(false);
+        img.gameObject.SetActive(false);
+    }
+
+    public void OnRetry()
+    {
+        panel.gameObject.SetActive(false);
+        player.constraints &= ~RigidbodyConstraints2D.FreezePosition;
+        button.gameObject.SetActive(true);
+        img.gameObject.SetActive(true);
+    }
+
+    public void OnExit()
+    {
+
+    }
+
+}
