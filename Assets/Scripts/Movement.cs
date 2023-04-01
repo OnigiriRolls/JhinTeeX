17b3629280472779bd50d938f14799c33150bd65
Onigiri <kethrinlie@yahoo.com>
2023-04-02 00:59:47 +0300

    Player move done

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float speed = 10f;
+    public float absSpeed = 10f;
     public ParticleSystem ps;
-
+    public float stickRadius;
+    private bool facingLeft = false;
+    private float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        absSpeed = Mathf.Abs(absSpeed);
         rb = GetComponent<Rigidbody2D>();
         ps.Emit(1);
         ps.Play();
@@ -25,12 +28,39 @@ public class Movement : MonoBehaviour
         horizontal1 = Input.GetAxis("Horizontal");
         vertical1 = Input.GetAxis("Vertical");
 
-        Vector2 position = rb.position; 
+         Vector2 position = rb.position; 
+
+        if (horizontal1 < (-1) * stickRadius)
+        {
+            //speed = (-1) * speed;
+
+            if (facingLeft == false)
+            {
+                Flip();
+            }
+        }
+        else if (horizontal1 > stickRadius)
+        {
+            speed = absSpeed;
+
+            if (facingLeft == true)
+            {
+                Flip();
+            }
+        }
 
         position.x = position.x + 0.1f * horizontal1 * Time.deltaTime * speed;
-        position.y = position.y + 0.1f * vertical1 * Time.deltaTime * speed;
-       
+        position.y = position.y + 0.1f * vertical1 * Time.deltaTime * absSpeed;
 
         rb.MovePosition(position);
     }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
 }
+
