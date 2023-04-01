67180c7775a2b378f7f126acbbdba4d544462b9e
Onigiri <kethrinlie@yahoo.com>
2023-04-02 00:24:34 +0300

    Pause, retry work

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
@@ -28,7 +29,7 @@ public class Pause : MonoBehaviour
 
     public void OnExit()
     {
-
+        SceneManager.LoadScene("Start");
     }
 
 }
